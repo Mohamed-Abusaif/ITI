0903ea1037369fb1ea39a900e4aa171162f9064e
Mohamed-Abusaif <mohamedabusaif50@gmail.com>
2024-12-27 16:19:10 +0200

    upload lab6 java and starter for lab7

--- java/Lab6/Lab6.java
+++ java/Lab6/Lab6.java
@@ -4,19 +4,28 @@ class Lab6 {
     static Map<Character, List<String>> map = new TreeMap<>();
 
     static void addWord(String word) {
-        char firstChar = word.charAt(0);
-
-        if (!map.containsKey(firstChar)) {
-            map.put(firstChar, new ArrayList<>());
+        char firstChar = Character.toLowerCase(word.charAt(0));
+        map.putIfAbsent(firstChar, new ArrayList<>());
+        List<String> words = map.get(firstChar);
+        if (!words.contains(word)) {
+            words.add(word);
+            Collections.sort(words);
         }
-        map.get(firstChar).add(word);
-
     }
 
     public static void main(String[] args) {
+        addWord("Apple");
+        addWord("Avocado");
+        addWord("Banana");
+        addWord("Blueberry");
+        addWord("Cherry");
+        addWord("Cucumber");
 
-        map.put(key, args)
+        System.out.println("Full Dictionary:");
+        for (Map.Entry<Character, List<String>> entry : map.entrySet()) {
+            System.out.println("Letter: " + entry.getKey() + ", Words: " + entry.getValue());
+        }
 
+        System.out.println("\nWords for letter 'B': " + map.getOrDefault('b', Collections.emptyList()));
     }
-
 }
