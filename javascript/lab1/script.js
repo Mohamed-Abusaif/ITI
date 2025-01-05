a8f19754ab78573659c6f22d5cfa573e21a576a6
Mohamed-Abusaif <mohamedabusaif50@gmail.com>
2025-01-05 16:56:33 +0200

    upload lab1 js except for last 2 quetions

---
+++ javascript/lab1/script.js
@@ -0,0 +1,78 @@
+console.log("Hello World!");
+
+//Check Palindrome:
+function isPalindrome(str) {
+  if (str === undefined || str === null) {
+    return false;
+  }
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let reversedStr = str.split("").reverse().join("");
+  return str === reversedStr;
+}
+// var s = "HeheH";
+// console.log(isPalindrome(s));
+
+console.log("-----------------------------");
+
+//Get Discounted Price:
+function discountedPrice(price, discount) {
+  if (
+    price === undefined ||
+    price === null ||
+    discount === undefined ||
+    discount === null
+  ) {
+    return "Invalid input";
+  }
+  if (price < 0 || discount < 0) {
+    return "Invalid input";
+  }
+  return price - (price * discount) / 100;
+}
+
+// var price = 100;
+// var discount = 10;
+// console.log(discountedPrice(price, discount));
+
+console.log("-----------------------------");
+
+//Array of Movies:
+var myMovies = [
+  "The Dark Knight",
+  "Pulp Fiction",
+  "Fight Club",
+  "Inception",
+  "The Matrix",
+];
+//a
+var myMoviesCopy = myMovies;
+//b
+myMoviesCopy[2] = "The Godfather";
+//c
+console.log(myMovies[myMovies.length - 1]);
+console.log(myMovies.slice(-1));
+console.log(myMovies.slice(4));
+//d
+myMovies.unshift("The Shawshank Redemption");
+
+console.log("-----------------------------");
+
+// Function that accept a sentence and return the longest word
+function longestWord(sentence) {
+  if (sentence === undefined || sentence === null) {
+    return "Invalid input";
+  }
+  var words = sentence.split(" ");
+  var longestWord = "";
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+}
+
+var sentence = "The quick brown fox jumped over the lazy dog";
+console.log(longestWord(sentence));
+
+console.log("-----------------------------");
