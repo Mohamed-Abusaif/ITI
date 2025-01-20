9abb83d3a258245c2382781c1f8745cb4c28e894
Mohamed Abusaif <67015050+Mohamed-Abusaif@users.noreply.github.com>
2025-01-20 02:43:00 +0200

    upload lab3 angular

--- angular/lab3/src/app/app.component.ts
+++ angular/lab3/src/app/app.component.ts
@@ -1,26 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from "./nav/nav.component";
+import { FooterComponent } from "./footer/footer.component";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, HomeComponent, NavComponent, FooterComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lab3';
 }
-
-
-//Routes to Be Added
-/*
-
-home 
-login
-register 
-cart 
-navbar
-list -> json products
-  
-*/
--- angular/lab3/src/app/app.routes.ts
+++ angular/lab3/src/app/app.routes.ts
@@ -1,3 +1,51 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: "home"
+    }
+    ,
+    {
+        path: 'product-list',
+        component: ListComponent,
+        title: "product-list"
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: "login"
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        title: "register"
+    },
+    {
+        path: 'cart',
+        component: CartComponent,
+        title: "cart"
+    },
+    {
+      path:"product-details/:id",
+      component: ProductDetailsComponent,
+      title: "product details"
+    }
+    ,
+    {
+        path: '**',
+        component: NotFoundComponent,
+        title: "not-found"
+    }
+];
+
+
+//product card has two buttons one for cart - details
